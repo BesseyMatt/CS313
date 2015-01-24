5a06dd019999c1d67c4fa234a664c98b4493b096
Matt Bessey <bes07001@byui.edu>
2015-01-24 16:51:40 -0700

    php form assignment

---
+++ phpsurvey.php
@@ -0,0 +1,72 @@
+<?php
+
+$hasSubmitted = "HasSubmitted";
+
+if(isset($_SESSION['user']) | isset($_COOKIE[$hasSubmitted]))
+{
+    header("Location: results.php");
+}
+
+session_start('user');
+$_SESSION['user'] = rand();
+
+?>
+
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <title>PHP Survey Assignment</title>
+        <link href='http://fonts.googleapis.com/css?family=Lora|Indie+Flower|Titillium+Web' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="phpsurvey.css">
+           
+        <script>
+         function seeResults() {
+             window.location.href = 'results.php';
+         }
+        </script>
+            
+        
+    </head>
+    <body>
+        
+        
+        <h1>PHP survey</h1>
+        <?php
+        $count = 2;
+                setcookie($hasSubmitted, $count, time() + 10 * 60);
+        ?>
+        
+        <form action="results.php" action="POST">
+            <h3>What is your Major?</h3>
+            <input type="radio" name='major' id='major' value='Computer Science' required> Computer Science</br>
+            <input type="radio" name='major' id='major' value='Computer Engineering' required> Computer Engineering</br>
+            <input type="radio" name='major' id='major' value='Computer Information Technology' required> Computer Information Technology</br>
+            <input type="radio" name='major' id='major' value='Web Design and Development' required> Web Design and Development</br> 
+            <input type="radio" name='major' id='major' value='Other'> Other</br></br>
+            
+            <h3>What Year of School are you in?</h3>
+            <input type="radio" name='year' id='year' value='Freshmen' required> Freshmen</br>
+            <input type="radio" name='year' id='year' value='Sophomore' required> Sophomore</br>
+            <input type="radio" name='year' id='year' value='Junior' required> Junior</br>
+            <input type="radio" name='year' id='year' value='Senior' required> Senior</br>
+            <input type="radio" name='year' id='year' value='Super Senior' required> Super Senior</br></br>
+            
+            <h3>Relationship status?</h3>
+            <input type="radio" name='status' id='staus' value='Married' required> Married</br>
+            <input type="radio" name='status' id='staus' value='Single' required> Single</br>
+            <input type="radio" name='status' id='staus' value='Dating' required> Dating</br>
+            <input type="radio" name='status' id='staus' value='It is Complicated' required> It's Complicated</br></br>
+            
+            <h3>Are you working while going to school?</h3>
+            <input type="radio" name='work' id='work' value='Yes' required> Yes</br>
+            <input type="radio" name='work' id='work' value='No' required> No</br></br>
+            
+            <input type="submit">
+            <button onclick='seeResults()'>See Results</button>
+        </form>
+        
+        
+        
+    </body>
+</html>
---
+++ results.php
@@ -0,0 +1,86 @@
+<?php
+
+    if (isset($_REQUEST["major"]) && isset($_REQUEST["year"]) &&  isset($_REQUEST["status"]) && isset($_REQUEST["work"]))
+        {
+        $major = $_REQUEST["major"];
+        $year = $_REQUEST["year"];
+        $status = $_REQUEST["status"];
+        $work = $_REQUEST["work"];
+        }
+       
+       $results = array("Computer Science\n" => 0,
+           "Computer Engineering\n" => 0,
+           "Computer Information Technology\n" => 0, 
+           "Web Design and Development\n" => 0,
+           "Other\n" => 0,
+           "Freshmen\n" => 0,
+           "Sophomore\n" => 0,
+           "Junior\n" => 0,
+           "Senior\n" => 0,
+           "Super Senior\n" => 0,
+           "Married\n" => 0,
+           "Single\n" => 0, 
+           "Dating\n" => 0, 
+           "It is Complicated\n" => 0, 
+           "Yes\n" => 0, 
+           "No\n" => 0);
+        $file = fopen("results.txt", "a+") or die("Unable to open file!");
+        $txt = $major . "\n" . $year . "\n" . $status . "\n" . $work . "\n";
+        fwrite($file, $txt);
+        fclose($file);
+        
+        $file = fopen("results.txt", "a+") or die("Unable to open file!");
+        $total = 0;
+        while ($line = fgets($file))
+        { 
+            $total++;
+            $results[$line]++;
+        }
+        $total = $total / 4;
+        
+       // $txt = $major . "\n" . $year . "\n" . $status . "\n" . $work . "\n";
+        //fwrite($file, $txt);
+        //fclose($file);
+        
+        $hasSubmitted = "HasSubmitted";
+        setcookie($hasSubmitted, rand(), time() + 10 * 60);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Form Results</title>
+        <link href='http://fonts.googleapis.com/css?family=Lora|Indie+Flower|Titillium+Web' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="phpsurvey.css">
+    </head>
+    <body>
+        <h1> Results of the Survey:</h1>
+        <div id='formResults'>
+            <?php
+          echo "<h2>Total Surveys Counted: " . $total . "</h2>";  
+          echo "<h3>Majors:</h3>";     
+          echo "<div><span class='percent'>" . (int)(($results["Computer Science\n"] / $total) * 100) . "%</span>  Computer Science: <span class='numbers'>" . $results["Computer Science\n"] . "</span></div>";
+          echo "<div><span class='percent'>" . (int)(($results["Computer Engineering\n"] / $total) * 100) . "%</span>  Computer Engineering: <span class='numbers'>" . $results["Computer Engineering\n"] . "</span></div>";
+          echo "<div><span class='percent'>" . (int)(($results["Computer Information Technology\n"] / $total) * 100) . "%</span>  Computer Information Technology: <span class='numbers'>" . $results["Computer Information Technology\n"] . "</span></div>"; 
+          echo "<div><span class='percent'>" . (int)(($results["Web Design and Development\n"] / $total) * 100) . "%</span>  Web Design and Development: <span class='numbers'>" . $results["Web Design and Development\n"] . "</span></div>";
+          echo "<div><span class='percent'>" . (int)(($results["Other Majors\n"] / $total) * 100) . "%</span>  Other Majors: <span class='numbers'>" . $results["Other\n"] . "</div>";
+          
+          echo "<h3>Year in School:</h3>"; 
+          echo "<div><span class='percent'>" . (int)(($results["Freshmen\n"] / $total) * 100) . "%</span>  Freshmen: <span class='numbers'>" . $results["Freshmen\n"] . "</span></div>";
+          echo "<div><span class='percent'>" . (int)(($results["Sophomore\n"] / $total) * 100) . "%</span>  Sophomore: <span class='numbers'>" . $results["Sophomore\n"] . "</span></div>";
+          echo "<div><span class='percent'>" . (int)(($results["Junior\n"] / $total) * 100) . "%</span>  Junior: <span class='numbers'>" . $results["Junior\n"] . "</span></div>";
+          echo "<div><span class='percent'>" . (int)(($results["Senior\n"] / $total) * 100) . "%</span>  Senior: <span class='numbers'>" . $results["Senior\n"] . "</span></div>";
+          echo "<div><span class='percent'>" . (int)(($results["Super Senior\n"] / $total) * 100) . "%</span>  Super Senior: <span class='numbers'>" . $results["Super Senior\n"] . "</span></div>";
+          
+          echo "<h3>Relationship Status:</h3>"; 
+          echo "<div><span class='percent'>" . (int)(($results["Married\n"] / $total) * 100) . "%</span>  Married: <span class='numbers'>" .$results["Married\n"] . "</span></div>";
+          echo "<div><span class='percent'>" . (int)(($results["Single\n"] / $total) * 100) . "%</span>  Single: <span class='numbers'>" . $results["Single\n"] . "</span></div>"; 
+          echo "<div><span class='percent'>" . (int)(($results["Dating\n"] / $total) * 100) . "%</span>  Dating: <span class='numbers'>" . $results["Dating\n"] . "</span></div>"; 
+          echo "<div><span class='percent'>" . (int)(($results["It is Complicated\n"] / $total) * 100) . "%</span>  It is Complicated: <span class='numbers'>" . $results["It is Complicated\n"] . "</span></div>";
+          
+          echo "<h3>Working while going to school:</h3>"; 
+          echo "<div><span class='percent'>" . (int)(($results["Yes\n"] / $total) * 100) . "%</span>  Yes: <span class='numbers'>" . $results["Yes\n"] . "</span></div>"; 
+          echo "<div><span class='percent'>" . (int)(($results["No\n"] / $total) * 100) . "%</span>  No: <span class='numbers'>" . $results["No\n"] . "</span></div>";  
+            ?>
+        </div>
+    </body>
+</html>
